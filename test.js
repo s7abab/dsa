f98a34e5a223a7d926ed72c3e673cb152fa4db97
Shabab <s7abab2k1@gmail.com>
2024-01-25 22:52:51 +0530

    trie

--- test.js
+++ test.js
@@ -1,55 +1,12 @@
-const arr = [3, 3, 1132, 41, 212, 412];
-
-// function bubbleSort(arr) {
-//   let swapped = true;
-//   do {
-//     swapped = false;
-//     for (let i = 0; i < arr.length; i++) {
-//       if (arr[i] > arr[i + 1]) {
-//         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-//         swapped = true;
-//       }
-//     }
-//   } while (swapped);
-// }
-
-// bubbleSort(arr);
-// console.log(arr);
-
-// function insertionSort(arr) {
-//   for (let i = 1; i < arr.length; i++) {
-//     let j = i - 1;
-//     let insertNumber = arr[i];
-//     while (j >= 0 && arr[j] > insertNumber) {
-//       arr[j + 1] = arr[j];
-//       j--;
-//     }
-//     arr[j + 1] = insertNumber;
-//   }
-// }
-
-// insertionSort(arr);
-// console.log(arr);
-
-// function quickSort(arr) {
-//   if (arr.length < 2) {
-//     return arr;
-//   }
-//   let pivot = arr[arr.length - 1];
-//   let left = [];
-//   let right = [];
-
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     if (arr[i] < pivot) {
-//       left.push(arr[i]);
-//     } else {
-//       right.push(arr[i]);
-//     }
-//   }
-
-//   return [...quickSort(left), pivot, ...quickSort(right)];
-// }
-
-// console.log(quickSort(arr));
-
-
+// Original array
+let keyValueArray = [
+    ['name', 'John'],
+    ['age', 25],
+    ['city', 'New York']
+  ];
+  
+  // Create an object from the array
+  let convertedObject = Object.fromEntries(keyValueArray)
+  // Display the resulting object
+  console.log(convertedObject);
+  
