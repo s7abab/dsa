c8393fbe0e5bbbe2412a3f8a6c59aa81cc335e40
Shabab <s7abab2k1@gmail.com>
2024-02-01 17:24:34 +0530

    graph all operations

--- Data Structures/Graph/graph.js
+++ Data Structures/Graph/graph.js
@@ -1,36 +1,117 @@
 class Graph {
-    constructor() {
-      this.vertices = new Map();
+  constructor() {
+    this.list = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.list[vertex]) {
+      this.list[vertex] = new Set();
     }
-  
-    addVertex(vertex) {
-      this.vertices.set(vertex, []);
+  }
+
+  addEdges(vertex1, vertex2) {
+    if (!this.list[vertex1]) {
+      this.addVertex(vertex1);
     }
-  
-    addEdge(vertex1, vertex2) {
-      this.vertices.get(vertex1).push(vertex2);
-      this.vertices.get(vertex2).push(vertex1);
+    if (!this.list[vertex2]) {
+      this.addVertex(vertex2);
     }
-  
-    printGraph() {
-      for (const [vertex, neighbors] of this.vertices) {
-        console.log(`${vertex} -> ${neighbors.join(', ')}`);
+    this.list[vertex1].add(vertex2);
+    this.list[vertex2].add(vertex1);
+  }
+
+  display() {
+    for (let vertex in this.list) {
+      console.log(vertex, "->", [...this.list[vertex]]);
+    }
+  }
+
+  hasEdge(vertex1, vertex2) {
+    if (this.list[vertex1].has(vertex2) && this.list[vertex2].has(vertex1)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.list[vertex1].delete(vertex2);
+    this.list[vertex2].delete(vertex1);
+  }
+
+  removeVertex(vertex) {
+    if (!this.list[vertex]) {
+      return;
+    }
+    for (let adjecentVertex of this.list[vertex]) {
+      this.removeEdge(vertex, adjecentVertex);
+    }
+    delete this.list[vertex];
+  }
+
+  // depth first search
+  dfs(startVertex) {
+    if (!this.list[startVertex]) {
+      console.log("Vertex not found in the graph");
+      return;
+    }
+
+    const stack = [];
+    const visited = {};
+
+    stack.push(startVertex);
+    visited[startVertex] = true;
+
+    while (stack.length !== 0) {
+      const currentVertex = stack.pop();
+      console.log(currentVertex);
+
+      for (let neighbor of this.list[currentVertex]) {
+        if (!visited[neighbor]) {
+          stack.push(neighbor);
+          visited[neighbor] = true;
+        }
       }
     }
   }
-  
-  // Example usage:
-  const graph = new Graph();
-  
-  graph.addVertex('A');
-  graph.addVertex('B');
-  graph.addVertex('C');
-  graph.addVertex('D');
-  
-  graph.addEdge('A', 'B');
-  graph.addEdge('B', 'C');
-  graph.addEdge('C', 'D');
-  graph.addEdge('D', 'A');
-  
-  graph.printGraph();
-  
+
+  bfs(startVertex) {
+    if (!this.list[startVertex]) {
+      console.log("Vertex not found in the graph");
+      return;
+    }
+
+    const queue = [];
+    const visited = {};
+
+    queue.push(startVertex);
+    visited[startVertex] = true;
+
+    while (queue.length !== 0) {
+      const currentVertex = queue.shift();
+      console.log(currentVertex);
+
+      for (let neighbor of this.list[currentVertex]) {
+        if (!visited[neighbor]) {
+          queue.push(neighbor);
+          visited[neighbor] = true;
+        }
+      }
+    }
+  }
+}
+
+const graph = new Graph();
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
+graph.addEdges(1, 2);
+graph.addEdges(3, 2);
+graph.addEdges(4, 3);
+graph.addEdges(1, 4);
+graph.addEdges(1, 5);
+// graph.dfs(2);
+
+graph.bfs(1);
