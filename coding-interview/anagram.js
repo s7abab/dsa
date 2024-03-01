57bee5f41317eda04102c9a0c239b91d27644e29
Shabab <s7abab2k1@gmail.com>
2024-03-01 21:07:29 +0530

    anagram

---
+++ coding-interview/anagram.js
@@ -0,0 +1,23 @@
+function areAnagrams(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const count = {};
+
+    for (let char of str1) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let char of str2) {
+        if (!count[char]) {
+            return false; 
+        }
+        count[char]--;
+    }
+
+    return true;
+}
+
+console.log(areAnagrams("listen", "silent")); 
+console.log(areAnagrams("hello", "world")); 
